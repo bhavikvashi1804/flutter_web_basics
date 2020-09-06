6c17ad46445ddff2c7ed09eeb7dad2ee087b39ad
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-09-06 18:07:52 +0530

    Created Navigation Bar

---
+++ lib/views/home/HomeView.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import '../../widgets/navigation_bar/navigation_bar.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          children: [
+            NavigationBar(),
+          ],
+        ));
+  }
+}
