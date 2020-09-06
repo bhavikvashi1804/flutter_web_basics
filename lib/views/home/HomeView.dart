ac8e8a91350001d2ff42df7c10c2f45b46a2b962
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-09-06 18:22:19 +0530

    Make the NavBar to center

--- lib/views/home/HomeView.dart
+++ lib/views/home/HomeView.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../widgets/navigation_bar/navigation_bar.dart';
+import '../../widgets/centered_view/centered_view.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key key}) : super(key: key);
@@ -8,11 +9,14 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        body: Column(
-          children: [
-            NavigationBar(),
-          ],
-        ));
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          CenteredView(
+            child: NavigationBar(),
+          ),
+        ],
+      ),
+    );
   }
 }
