6beb59c332ae1f0299eff2048b586357cf06fd40
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-09-06 18:59:44 +0530

    Display the nav bar responsive

--- lib/widgets/navigation_bar/navigation_bar.dart
+++ lib/widgets/navigation_bar/navigation_bar.dart
@@ -1,31 +1,19 @@
 import 'package:flutter/material.dart';
 
-import './navbar_logo.dart';
-import './navbar_item.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+
+import './navigation_bar_mobile.dart';
+import './navigation_bar_tablet_desktop.dart';
 
 class NavigationBar extends StatelessWidget {
   const NavigationBar({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          NavBarLogo(),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              NavBarItem('Episodes'),
-              SizedBox(
-                width: 60,
-              ),
-              NavBarItem('About'),
-            ],
-          )
-        ],
-      ),
+    return ScreenTypeLayout(
+      mobile: NavigationBarMobile(),
+      tablet: NavigationBarTabletDesktop(),
+      desktop: NavigationBarTabletDesktop(),
     );
   }
 }
