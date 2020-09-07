30ea9d8e69ce7ca702d09a01041521aec2274ef9
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-09-07 09:36:20 +0530

    Created NavBar Item responsive

---
+++ lib/widgets/navbar_item/navbar_item_desktop.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import '../../datamodels/navbar_item_model.dart';
+
+class NavBarItemTabletDesktop extends StatelessWidget {
+  final NavBarItemModel model;
+  NavBarItemTabletDesktop({this.model});
+
+  @override
+  Widget build(
+    BuildContext context,
+  ) {
+    return Text(
+      model.title,
+      style: TextStyle(fontSize: 18),
+    );
+  }
+}
